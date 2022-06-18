7515df68cb14d4ccb90a4e88b4684405ca7481d9
Jason Smit <38807670+Binxlola@users.noreply.github.com>
2022-06-19 01:44:14 +1200

    package.json update

--- src/app/mainNav.struct.ts
+++ src/app/mainNav.struct.ts
@@ -7,79 +7,79 @@ let navItems: NavItem[] = [
     routerLink: '/home',
     linkActiveClass: 'nav-btn-active',
   },
-  // {
-  //   id: 'rosters',
-  //   label: 'Rosters',
-  //   linkActiveClass: 'nav-btn-active',
-  //   subItems: [
-  //     {
-  //       id: '671',
-  //       label: '671',
-  //       routerLink: '/671',
-  //       linkActiveClass: 'nav-btn-active',
-  //     },
-  //     {
-  //       id: '672',
-  //       label: '672',
-  //       routerLink: '/672',
-  //       linkActiveClass: 'nav-btn-active',
-  //     }
-  //   ]
-  // },
-  // {
-  //   id: 'checks',
-  //   label: 'Appliance Checks',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'contacts',
-  //   label: 'Contacts',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'profile',
-  //   label: 'Profile',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'stats',
-  //   label: 'Stats',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'notices',
-  //   label: 'Notices',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'trailer',
-  //   label: 'Trailer',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'help',
-  //   label: 'Help',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'issues',
-  //   label: 'Known Issues',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
-  // {
-  //   id: 'logout',
-  //   label: 'Logout',
-  //   routerLink: '/',
-  //   linkActiveClass: 'nav-btn-active',
-  // },
+  {
+    id: 'rosters',
+    label: 'Rosters',
+    linkActiveClass: 'nav-btn-active',
+    subItems: [
+      {
+        id: '671',
+        label: '671',
+        routerLink: '/671',
+        linkActiveClass: 'nav-btn-active',
+      },
+      {
+        id: '672',
+        label: '672',
+        routerLink: '/672',
+        linkActiveClass: 'nav-btn-active',
+      }
+    ]
+  },
+  {
+    id: 'checks',
+    label: 'Appliance Checks',
+    routerLink: '/checks',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'contacts',
+    label: 'Contacts',
+    routerLink: '/contacts',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'profile',
+    label: 'Profile',
+    routerLink: '/profile',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'stats',
+    label: 'Stats',
+    routerLink: '/stats',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'notices',
+    label: 'Notices',
+    routerLink: '/notices',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'trailer',
+    label: 'Trailer',
+    routerLink: '/trailer',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'help',
+    label: 'Help',
+    routerLink: '/help',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'issues',
+    label: 'Known Issues',
+    routerLink: '/issues',
+    linkActiveClass: 'nav-btn-active',
+  },
+  {
+    id: 'logout',
+    label: 'Logout',
+    routerLink: '/logout',
+    linkActiveClass: 'nav-btn-active',
+  },
 ];
 
 export const mainNavItems = navItems;
