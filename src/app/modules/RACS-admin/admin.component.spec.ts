d0a5d81a5eb06775f6b2f8ad5bff1f12fa47f6c9
Jason Smit <38807670+Binxlola@users.noreply.github.com>
2022-06-30 22:59:14 +1200

    UERP-19 Setup RACS admin module

--- src/app/modules/RACS-admin/admin.component.spec.ts
+++ src/app/modules/RACS-admin/admin.component.spec.ts
@@ -3,10 +3,12 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AdminComponent} from './admin.component';
 import {RacsAdminModule} from "./racs-admin.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
+  let observer: BreakpointObserver;
   let compiled: any;
   const tabLabelIconPairs: { [key: string]: string; } = {
     Membership: 'groups',
@@ -19,12 +21,14 @@ describe('AdminComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [AdminComponent],
       imports: [RacsAdminModule, BrowserAnimationsModule],
+      providers: [BreakpointObserver]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(AdminComponent);
     compiled = fixture.nativeElement;
     component = fixture.componentInstance;
+    observer = TestBed.inject(BreakpointObserver);
     fixture.detectChanges();
   });
 
@@ -32,7 +36,7 @@ describe('AdminComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show tab labels in Web View', function () {
+  it('should show tab labels and icons', function () {
     let tabGroup = compiled.querySelector('mat-tab-group');
     let tabs: any[] = compiled.querySelectorAll('[role="tab"]');
     expect(component).toBeTruthy();
@@ -44,8 +48,14 @@ describe('AdminComponent', () => {
       const label: string = tab.querySelector('mat-label').innerText;
       const icon: string = tab.querySelector('mat-icon').innerText;
 
-      expect(label in tabLabelIconPairs).toBeTruthy();
+      // Only check label if in web view
+      if (observer.isMatched(Breakpoints.Web)) {
+        expect(label in tabLabelIconPairs).toBeTruthy();
+        console.log('got here');
+      }
+
+      // Check icons
       expect(tabLabelIconPairs[label]).toBe(icon)
-    })
+    });
   });
 });
