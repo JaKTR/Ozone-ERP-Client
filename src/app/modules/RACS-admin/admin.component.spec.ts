ab574abc95552e922a73f0bff011b75f4ba6b4d0
Jason Smit <38807670+Binxlola@users.noreply.github.com>
2022-07-01 14:40:38 +1200

    UERP-19 Setup RACS admin module

--- src/app/modules/RACS-admin/admin.component.spec.ts
+++ src/app/modules/RACS-admin/admin.component.spec.ts
@@ -21,6 +21,8 @@ describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
   let observer: BreakpointObserver;
+  let tabGroup: any;
+  let tabs: any[];
   let compiled: any;
   const tabLabelIconPairs: { [key: string]: string; } = {
     Membership: 'groups',
@@ -41,38 +43,56 @@ describe('AdminComponent', () => {
     compiled = fixture.nativeElement;
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    // Set up elements that will be used for multiple individual tests
+    tabGroup = compiled.querySelector('mat-tab-group');
+    tabs = compiled.querySelectorAll('[role="tab"]');
   });
 
-  it('should create and show tabs with labels in Web View', () => {
+  /**
+   * Test that the component is created successfully.
+   */
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  /**
+   * MUST RUN BEFORE INDIVIDUAL TAB TESTS
+   * Tests that the main tabs for the admin component are rendered,
+   */
   it('should show tabs with icons in Web View', waitForAsync(() => {
-      let tabGroup = compiled.querySelector('mat-tab-group');
-      let tabs: any[] = compiled.querySelectorAll('[role="tab"]');
+      // Check correct number of tabs
       expect(component).toBeTruthy();
       expect(tabGroup).toBeTruthy();
       expect(tabs.length).toBe(Object.keys(tabLabelIconPairs).length);
-
-      fixture.whenStable().then(() => {
-        spyOn(observer, 'observe').and.returnValue(of({
-          matches: true,
-          breakpoints: {
-            "web": true
-          }
-        } as BreakpointState));
-
-        fixture.detectChanges();
-        expect(observer.observe).toHaveBeenCalled();
-        tabs.forEach((tab: any) => {
-          const label: string = tab.querySelector('mat-label').innerText;
-          const icon: string = tab.querySelector('mat-icon').innerText;
-
-          // Check labels and icons for each tab
-          expect(label in tabLabelIconPairs).toBeTruthy();
-          expect(tabLabelIconPairs[label]).toBe(icon)
-        });
-      });
     })
   );
+
+  /**
+   * Tests that each of the main tabs for the admin component have the correct icon and label.
+   */
+  it('Should show the correct tab labels and icons in Web View', waitForAsync(() => {
+    fixture.whenStable().then(() => {
+      spyOn(observer, 'observe').and.returnValue(of({
+        matches: true,
+        breakpoints: {
+          "web": true
+        }
+      } as BreakpointState));
+
+      // Wait for changes and confirm observe was called
+      fixture.detectChanges();
+      expect(observer.observe).toHaveBeenCalled();
+
+      // Check correct labels and icons
+      tabs.forEach((tab: any) => {
+        const label: string = tab.querySelector('mat-label').innerText;
+        const icon: string = tab.querySelector('mat-icon').innerText;
+
+        // Check labels and icons for each tab
+        expect(label in tabLabelIconPairs).toBeTruthy();
+        expect(tabLabelIconPairs[label]).toBe(icon)
+      });
+    });
+  }));
 });
